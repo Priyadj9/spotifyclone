4b0cf11d2a24e32d66356bab938e8104d332d3dc
Priyadarshana Jain <priya@Priyadarshanas-MacBook-Air.local>
2024-10-08 13:34:13 +0530

    cors

--- spotify-backend/server.js
+++ spotify-backend/server.js
@@ -15,7 +15,26 @@ connectCloudinary();
 
 // MIDDLEWARES
 app.use(express.json());
-app.use(cors());
+
+const allowedOrigins = [
+    'https://priyadjain9spotify.netlify.app',
+    'https://priyadjain9spotifyadmin.netlify.app/'
+  ];
+  
+const corsOptions = {
+    origin: function (origin, callback) {
+     
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    }
+  };
+
+  app.use(cors(corsOptions));
+
 
 // INITIALIZING ROUTES
 app.use("/api/song",songRouter);
