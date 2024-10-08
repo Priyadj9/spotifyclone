2eaf400b25429f7ac6f6be9d25bfbde22661ca52
Priyadarshana Jain <priya@Priyadarshanas-MacBook-Air.local>
2024-10-08 12:39:42 +0530

    correction

--- spotify-backend/server.js
+++ spotify-backend/server.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import 'dotenv/config';
 import songRouter from './src/routes/songRoute.js';
 import albumRouter from './src/routes/albumRoute.js';
-import connectDB from './src/config/mongoDb.js';
+import connectDB from './src/config/mongodb.js';
 import connectCloudinary from './src/config/cloudinary.js';
 
 //APP CONFIG
